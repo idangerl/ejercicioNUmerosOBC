35c0cff06f38051471dfc1d80bbd70892a526f25
Oscar Daniel <92224083+idangerl@users.noreply.github.com>
2022-11-09 12:22:32 -0400

    ejercicio numeros

---
+++ ejercicio.js
@@ -0,0 +1,11 @@
+let alturaCen = 166
+let alturaMt = 1.66
+let pesoKg = 68.60
+
+let alturaRedondeada = Math.ceil(alturaMt)
+let pesoRedondeado = Math.floor(pesoKg)
+let maximoValor = Number.MAX_VALUE+1 === Number.MAX_VALUE
+
+console.log(alturaRedondeada)
+console.log(pesoRedondeado)
+console.log(maximoValor)
